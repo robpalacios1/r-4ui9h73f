5c86a8eff92ba0fd6ff6eb8e5c6037eb954f67e3
Roberto <robpalacios1@hotmail.com>
2019-05-28 11:33:52 -0500

    repeat a string

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+	  super();
+	  this.state = {
+	  	count: 0
+	}
+}
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.updateCount.bind(this)} />
+        <p className="repeater">{this.state.count}</p>
       </div>
     );
+  }
+
+  updateCount(e){
+  	this.setState({
+  		count: e.target.value
+  	});
   }
 }
 
